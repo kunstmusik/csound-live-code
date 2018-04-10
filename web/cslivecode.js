8521f00894b7938a7a70c415334127011213395f
Steven Yi <stevenyi@gmail.com>
2018-04-09 21:35:17 -0400

    experimenting with making site a progressive web application

--- web/cslivecode.js
+++ web/cslivecode.js
@@ -105,3 +105,10 @@ Module['onRuntimeInitialized'] = onRuntimeInitialized;
 window.onbeforeunload = function() {
   return "Are you...sure?" 
 };
+
+// ServiceWorker for PWA
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./service-worker.js')
+    .then(function() { console.log('Service Worker Registered'); });
+}
